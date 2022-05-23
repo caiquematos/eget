417f9b24a5a1d0d030533b28aea690550ef75d13
Caique Pires <caiquepires91@gmail.com>
2022-05-23 08:47:07 -0300

    Logo nos emails

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,11 +1,8 @@
 <tr>
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
-@if (trim($slot) === 'Laravel')
-<img src="https://laravel.com/img/notification-logo.png" class="logo" alt="Laravel Logo">
-@else
+<img src="{{url('icon.png')}}" class="logo" alt="CDI logo">
 {{ $slot }}
-@endif
 </a>
 </td>
 </tr>
