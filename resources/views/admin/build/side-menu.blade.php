f1d743b403ef598c64aabf1c1dd84e901492fa5e
Caique Pires <caiquepires91@gmail.com>
2022-05-23 15:32:36 -0300

    ajusto logo admin

--- resources/views/admin/build/side-menu.blade.php
+++ resources/views/admin/build/side-menu.blade.php
@@ -1,7 +1,7 @@
 <div class="col-md-3 left_col">
     <div class="left_col scroll-view">
         <div class="navbar nav_title" style="border: 0;">
-        <a href="{{route('admin.home')}}" class="site_title"><i class="fa fa-paw"></i> <span>CDI</span></a>
+        <a href="{{route('admin.home')}}" class="site_title"><img src="{{url('icon30.png')}}" width="29" alt="CDI - Cartão de Vantages" style="margin-right: 1px;margin-top: -6px">  <span style="font-size: large">Cartão de Vantagens</span></a>
         </div>
 
         <div class="clearfix"></div>
