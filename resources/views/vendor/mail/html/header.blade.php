87c1f99fb945073f2656456d6853a4bbeb56a98d
Caique Pires <caiquepires91@gmail.com>
2022-05-23 09:25:24 -0300

    ajuste logo

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,10 +1,8 @@
 <tr>
 <td class="header" style="position: relative;height:105px;">
 <a href="{{ $url }}" style="position: absolute;left: 50%;top: 50%;transform: translate(-50%, -50%);">
-    <div style="position: relative;">
-        <img width="40" height="42" src="{{url('icon.png')}}" class="logo" alt="CDI logo" style="width:40px; height:42px;transform: translateY(-7px);">
-        <span style="margin-left: 50px">{{ $slot }}</span>
-    </div>
+<img width="40" height="42" src="{{url('icon.png')}}" class="logo" alt="CDI logo" style="width:40px; height:42px;">
+<span style="margin-left: 10px">{{ $slot }}</span>
 </a>
 </td>
 </tr>
