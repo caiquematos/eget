e27e0e947a494f1ceab167b8fdc527be6da23965
Caique Pires <caiquepires91@gmail.com>
2022-05-23 08:56:28 -0300

    Logo nos emails

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,8 +1,8 @@
 <tr>
 <td class="header">
-<a href="{{ $url }}" style="display: inline-block;">
+<a href="{{ $url }}" style="display: flex;flex-direction: row;align-content: center;align-items: center;">
 <img src="{{url('icon.png')}}" class="logo" alt="CDI logo">
-{{ $slot }}
+<span style="margin-left: 10px">{{ $slot }}</span>
 </a>
 </td>
 </tr>
