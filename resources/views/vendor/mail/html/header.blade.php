05dab9c7701f1c53fb41ab07f16ccc8e2559fc7d
Caique Pires <caiquepires91@gmail.com>
2022-05-23 09:09:27 -0300

    ajuste logo email

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,7 +1,7 @@
 <tr>
-<td class="header">
-<a href="{{ $url }}" style="display: flex;flex-direction: row;align-content: center;align-items: center;">
-<img width="40" height="42" src="{{url('icon.png')}}" class="logo" alt="CDI logo">
+<td class="header" style="position: relative;height:105px;">
+<a href="{{ $url }}" style="position: absolute;left: 50%;top: 50%;transform: translate(-50%, -50%);">
+<img width="40" height="42" src="{{url('icon.png')}}" class="logo" alt="CDI logo" style="width:40px; height:42px;margin-bottom:-13px;">
 <span style="margin-left: 10px">{{ $slot }}</span>
 </a>
 </td>
